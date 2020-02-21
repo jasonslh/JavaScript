cbbd96d6186ecaedd65f2b6d5c185e809828c148
jasonslh <61194484+jasonslh@users.noreply.github.com>
2020-02-21 10:35:11 +0000

    Initial Java Script Upload

---
+++ intro.js
@@ -0,0 +1,101 @@
+//let variable ='Hello, world!'
+//    console.log(variable)
+//let name = 'Jason';
+//let favDrink ='Coffee';
+
+//console.log(`My name is ${name} and my favourite drink is ${favDrink}.`)
+
+//if (name == 'Jason') {
+//    console.log(`Great Name!`)
+//    }
+//else if(name == 'Stuart') {
+//    console.log(`Less great name`)
+//}
+//else {
+//    console.log(`Oh dear...`)
+//}
+
+//let age =22
+//let country ='UK'
+
+//if(age>22 && country =='UK') {
+//    console.log(`I can serve you in this country.`)
+//}
+//else{
+//    console.log(`You are not old enough, or in the wrong country.`)
+//}
+
+//let name ='Jason'
+
+//if(name =='Jason'|| name =='Sam') {
+//    console.log(`You are either Jason or Sam!`)
+//}
+//else{
+//    console.log(`I don't know you...`)
+//}
+
+//for i in range(5):
+//print(i)
+//for (i=0;i<5;i++){
+//    console.log(i)
+//}
+
+
+//names = ['Sam', 'Stuart', 'Liam', 'Ezra']
+//for i in names:
+//    print(i)
+//let names = [`Sam`, `Stuart`, `Liam`, `Ezra`]
+//for (i=0; i< names.length;i++){
+//    console.log(names[i])
+//}
+
+//def my_func(aString);
+    //print(aString)
+//my_func('hello')
+
+
+
+//const my_func=(aString)=>{
+//    console.log(aString)
+//}
+//my_func=`Hello`
+
+// Arrow function syntax
+const myFunc = (aString) => {
+    console.log(aString)
+}
+​
+myFunc('Arrow functiosn syntax')
+​
+​
+// Function declaration
+function myOtherFunc (anotherString) {
+    console.log(anotherString)
+}
+​
+myOtherFunc("Function declaration")
+Collapse
+
+
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random() * 10))
+
+function checkPassword (inputtext) {
+}
+let name = 'password'
+    if (password < 8) {
+        console.log(`password is too short.`)
+    }
+    else {
+        console.log(`password.`)
+}
+
+//if (name == 'Jason') {
+//    console.log(`Great Name!`)
+//    }
+//else if(name == 'Stuart') {
+//    console.log(`Less great name`)
+//}
+//else {
+//    console.log(`Oh dear...`)
+//}
